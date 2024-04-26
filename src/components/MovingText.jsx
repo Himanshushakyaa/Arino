3d2d8d786a6794de0b1a55e393206f645bcc776f
Himanshu <hs3928782@gmail.com>
2024-04-26 11:03:09 +0530

    text

--- src/components/MovingText.jsx
+++ src/components/MovingText.jsx
@@ -4,7 +4,7 @@ const MovingText=()=> {
   return (
     <div className='relative overflow-hidden h-40'>
 <div className='absolute inset-x-0 animation-scrolling-text'>
-    <span className=' text-transparent lg:px-4 py-2 lg:text-4xl text-4xl font-semibold uppercase
+    <span className=' text-transparent lg:px-4 py-2 lg:text-6xl text-4xl font-semibold uppercase
     text-stroke-3'> Our reputed world wide partners!!</span>
 </div>
     </div>
